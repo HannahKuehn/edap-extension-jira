a98dee29579769e7fdd126081ebb2ac1b7b3f157
hk <hannah.kuehn@protonmail.com>
2021-02-05 14:57:30 +0100

    add summary to checklist output

--- edap_plugin/edap_plugin/public/js/addon.js
+++ edap_plugin/edap_plugin/public/js/addon.js
@@ -1,59 +1,76 @@
 /* App frontend script */
 var rememberData;
 
-var buttonClick = function(groups){
-      rememberData = groups;
-      var out = "";
+var buttonClick = function (groups) {
+  rememberData = groups;
+  var out = "";
 
-      for(var i = 0; i < groups.length; i++){
-        var data = groups[i].questions;
-        out += "<H2>" + groups[i].group + "</H2><br>";
-        for(var q = 0; q < data.length; q++){
-          if(document.getElementById("question" + data[q].qid).checked){
-            out += "<H3>" +  data[q].question + "</H3><br>";
-            out += data[q].consequence + "<br>";
-          }
-  
-          out += "<ul>";
-  
-          for(var qs = 0; qs < data[q].subquestions.length; qs++){
-            if(document.getElementById("subquestion" + data[q].subquestions[qs].qid).checked){
-              out += "<li><H4>" + data[q].subquestions[qs].question + "</H4><br>";
-              out += data[q].subquestions[qs].consequence + "<br></li>";
-            }
-  
-          }
-  
-          out += "</ul>";
-        
-        }
+  for (var i = 0; i < groups.length; i++) {
+    var data = groups[i].questions;
+    out += "<H2>" + groups[i].group + "</H2><br>";
+
+    for (var q = 0; q < data.length; q++) {
+      if (document.getElementById("question" + data[q].qid).checked) {
+        out += questionFormat(false, data[q].question, data[q].short, data[q].consequence);
       }
-      
-      document.getElementById("pagetitle").textContent = "Result of your checklist";
-      document.getElementById("checklist").style.display = "none";
-      document.getElementById("ausgabetext").innerHTML = out;
-      document.getElementById("result").style.visibility = "visible";
-  };
 
-  var group = function(group){
-    document.getElementById("groupdiv" + group).style.display = document.getElementById("group" + group).checked ? "inline" : "none";
-  };
+      out += "<ul>";
 
-  var back = function(){
-    for(var i = 0; i < rememberData.length; i++){
-      var data = rememberData[i].questions;
-      document.getElementById("group" + rememberData[i].groupid).checked = false;
-      document.getElementById("groupdiv" + rememberData[i].groupid).style.display = "none";
-      for(var q = 0; q < data.length; q++){
-        document.getElementById("question" + data[q].qid).checked = false;
-        for(var qs = 0; qs < data[q].subquestions.length; qs++){
-          document.getElementById("subquestion" + data[q].subquestions[qs].qid).checked = false;
+      for (var qs = 0; qs < data[q].subquestions.length; qs++) {
+        if (document.getElementById("subquestion" + data[q].subquestions[qs].qid).checked) {
+          out += questionFormat(true, data[q].subquestions[qs].question, data[q].subquestions[qs].short, data[q].subquestions[qs].consequence);
         }
+
+      }
+
+      out += "</ul>";
+
+    }
+  }
+  document.getElementById("pagetitle").textContent = "Result of your checklist";
+  document.getElementById("checklist").style.display = "none";
+  document.getElementById("ausgabetext").innerHTML = out;
+  document.getElementById("result").style.visibility = "visible";
+};
+
+var questionFormat = function(sub, question, short, consequence){
+  var beginHeader = sub? "<li><H4>" : "<H3>";
+  var endHeader = sub? "</H4><br>" : "</H2><br>";
+  var end = sub? "</li><br>" : "<br>";
+  var str = "";
+  str += beginHeader + question + endHeader;
+
+  for(var i = 0; i < short.length; i++){
+    str += "<li>" + short[i] + "</li><br>";
+  }
+  
+  str += consequence + end;
+  return str;
+};
+
+var subFormat = function(question, short, consequence){
+
+}
+
+var group = function (group) {
+  document.getElementById("groupdiv" + group).style.display = document.getElementById("group" + group).checked ? "inline" : "none";
+};
+
+var back = function () {
+  for (var i = 0; i < rememberData.length; i++) {
+    var data = rememberData[i].questions;
+    document.getElementById("group" + rememberData[i].groupid).checked = false;
+    document.getElementById("groupdiv" + rememberData[i].groupid).style.display = "none";
+    for (var q = 0; q < data.length; q++) {
+      document.getElementById("question" + data[q].qid).checked = false;
+      for (var qs = 0; qs < data[q].subquestions.length; qs++) {
+        document.getElementById("subquestion" + data[q].subquestions[qs].qid).checked = false;
       }
     }
-    document.getElementById("checklist").style.display = "inline";
-    document.getElementById("result").style.visibility = "collapse";
-    document.getElementById("pagetitle").textContent = "Welcome to the EDAP Checklist";
-  };
+  }
+  document.getElementById("checklist").style.display = "inline";
+  document.getElementById("result").style.visibility = "collapse";
+  document.getElementById("pagetitle").textContent = "Welcome to the EDAP Checklist";
+};
 
 
--- edap_plugin/edap_plugin/routes/questionsjson.js
+++ edap_plugin/edap_plugin/routes/questionsjson.js
@@ -7,34 +7,40 @@ const data = {
         {
           qid: "0_0",
           question: "The data is collected from various different data sources",
+          short: ["conduct data inventory"],
           consequence: "Make sure you know the provenance and lineage of the data (origins of data, and how and why it was collected), especially when data is from an outside source. When working with multiple sources, it could be helpful to conduct a data inventory to help you identify, organise and describe the different data sources you collect, access and maintain",
           subquestions: []
         },
         {
           qid: "0_1",
           question: "You are not sure whether the data is biased or not",
+          short: ["biased behaviour can lead to illegal discriminatory practices", "convene with people from different perspectives and backgrounds within your team"],
           consequence: "Bias – whether intentional or not – always exists. It can be encoded into data. Sometimes biased behaviour can lead to illegal discriminatory practices. However, actively counteracting societal bias is often thought of as fair, for example positive discrimination policies to counter under-representation of certain groups. Making encoded bias and other limitations visible will help you identify those impacted, understand the consequences, and consider mitigations as necessary. <br> Bias can creep into your project anywhere decisions are made about the data to collect, the training data to use, and the analysis model. This includes a product design team’s decision to include (or fail to consider) a particular group's perspective eg the elderly. <br> Bias can be hard to detect, so it helps to be upfront about your project’s limitations ie who is not represented, whether and where you have used proxy data, and your basis for making assumptions. Convening people with different perspectives and backgrounds within your team, and through external engagement. might also help to flag potential limitations and address sources of bias.",
           subquestions: []
         },
         {
           qid: "0_2",
           question: "The data will be shared with other organisations or published",
+          short: ["check for conditions or aggreements covering data sharing"],
           consequence: "Are there conditions or agreements covering data sharing which might affect your project? When accessing or planning to share data with other organisations, this might be governed by an agreement or an industry code of practice.",
           subquestions: []
         },
         {
           qid: "0_3",
           question: "The data is collected directly from individuals",
+          short: ["consider individuals rights over their data", "consider how they would feel about the use of their data"],
           consequence: "Individuals may have rights over the data, or be contributing data that might identify or impact other individuals. As well as legal rights, consider the expectations of individuals and how comfortable they would feel with proposed data uses. For instance, data obtained from public sources eg social media sites or through web scraping should be used responsibly.",
           subquestions: [
             {
               qid: "0_3_0",
               question: "The data will be reused or re-purposed",
-              consequence: "You may need to go back and ask for permission or reassess your plans"
+              short: ["You may need to go back and ask for permission or reassess your plans"],
+              consequence: ""
             },
             {
               qid: "0_3_1",
               question: "The purpose of the collection of data is not clearly communicated to the user",
+              short: ["communicate what you want to do with the data", "communicate risks and harms", "make communication accessible"],
               consequence: "Especially when planning to collect personal data, you need to be able to explain your intention and what you plan to do with the data e.g how long you will keep it. The data should be relevant to what you are trying to achieve. Do not only communicate benefits. Communicate potential risks and harms too. This will help people meaningfully engage with you. <br>Make sure you are communicating in short and accessible sentences ie not lengthy disclaimers and consents. Your communications should be developed and tested with people. "
             }
           ]
@@ -42,6 +48,7 @@ const data = {
         {
           qid: "0_4",
           question: "You have not yet reviewed the legal requirements and ethical codes that apply to your project",
+          short: ["consider legal requirements", "consider broader social outcomes"],
           consequence: "Are you, or members of your team aware of any legal requirements or ethical frameworks that might apply to the collection, processing and use of data in your project? If working with personal data, you need to be aware of requirements within your context eg in the GDPR in the EU. <br> Also think beyond traditional data protection laws and consider the broader social outcomes of your project, and how they could impact on people and human rights. For example, could certain analysis models or automated decision-making lead to discrimination against particular groups? Or would your project reduce access to services for more vulnerable customers? <br> Beyond regulations, there may be other frameworks or codes of practice you  should consider. Many of these apply to specific sectors eg financial regulations, medical association codes, or guidelines for statisticians.",
           subquestions: []
         }
